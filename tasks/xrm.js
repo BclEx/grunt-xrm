019fe89c14598529a7dc00049aefef0df257854b
sky.morey <smorey@degdigital.com>
2015-07-29 15:53:00 -0500

    updates

--- tasks/xrm.js
+++ tasks/xrm.js
@@ -10,23 +10,32 @@
 
 // External libs.
 var path = require('path');
-var yeoman = require('yeoman-generator');
+var yeoman = require('yeoman-environment');
+var env = yeoman.createEnv();
+env.register(require.resolve('generator-xrm'));
 
 module.exports = function (grunt) {
 
-    var self = this;
-    grunt.registerMultiTask('xrm', 'test', function () {
+    // Default date format logged
+    var process = function (arg, dest) {
+        env.run(['xrm', arg], { 'skip-install': true }, function() { });
+        //grunt.log.writeln(dest);
+    };
+
+    grunt.registerMultiTask('xrm', 'runs tests', function () {
 
         // Merge task-specific and/or target-specific options with these defaults.
         var options = this.options({
             punctuation: '.',
         });
 
+        //console.log(this.files);
+
         // Iterate over all specified file groups.
         this.files.forEach(function (file) {
             file.src.filter(function (filepath) { return (grunt.file.exists(filepath)); }).forEach(function (filepath) {
                 // Read file source.
-                self.process(grunt.file.readJSON(filepath), file.dest);
+                process(grunt.file.readJSON(filepath), file.dest);
                 // Print a success message.
                 grunt.log.writeln('File "' + path.basename(filepath) + '" processed.');
             });
